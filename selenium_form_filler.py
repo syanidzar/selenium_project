ea9bd20dd9e87fc9462a442fcb717cacb5828e40
Syanidzar bin Shahidan <syanidzar@proton.me>
2024-11-21 13:59:36 +0800

    remove unused imports

--- selenium_form_filler.py
+++ selenium_form_filler.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 import csv
 from pynput import keyboard
